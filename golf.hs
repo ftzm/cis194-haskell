65692d6ca6393759c9c7b71efd71f85cd45bc31d
matt <fitz.matt.d@gmail.com>
2016-04-02 13:01:01 +0200

    add solution for homework 3

---
+++ golf.hs
@@ -0,0 +1,29 @@
+module Golf where
+import Data.List
+import Data.List.Split.Internals
+import Safe
+
+
+
+skips :: [a] -> [[a]]
+skips xs = map (lastDef [] . transpose. flip chunksOf xs) [1..length xs]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:r)
+  | y > x && y > z = y : localMaxima (y:z:r)
+  | otherwise = localMaxima (y:z:r)
+localMaxima _ = []
+
+histogram :: [Integer] -> String
+histogram xs = unlines ((
+            transpose
+            $ map (reverse . \x -> concat $ replicate x "*" ++ replicate (9-x) " ")
+            $ foldr (\x acc -> length ( filter (==x) xs ) : acc) [] [0..9] )
+            ++ ["==========", "0123456789"])
+
+
+a :: [Integer]
+a = [1,4,5,4,6,6,3,4,2,4,9]
+
+main :: IO ()
+main = putStrLn $ histogram a
